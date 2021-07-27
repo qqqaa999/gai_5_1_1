5f2040aa8ee1a94b12489b36a8267403a46b1c95
qqqaa999 <qqqaa999@naver.com>
2021-07-27 16:05:21 +0900

    2021-7-27

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -4,6 +4,6 @@ from profileapp.views import ProfileCreateView
 app_name = 'profileapp'
 
 urlpatterns = [
-    path('create/', ProfileCreateView.as_view(), name='create')
-
+    path('create/', ProfileCreateView.as_view(), name='create'),
+    path('update/<ink:pk>', ProfileCreateView.as_view(), name='update'),
 ]
