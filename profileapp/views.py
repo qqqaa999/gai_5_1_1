86da94a70655597d670255771f275b8155c744ac
qqqaa999 <qqqaa999@naver.com>
2021-07-29 09:19:01 +0900

    2021-7-29

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,8 +1,7 @@
-from bs4.diagnose import profile
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView
 
 from profileapp.forms import ProfileCreationForm
@@ -10,18 +9,23 @@ from profileapp.models import Profile
 
 
 class ProfileCreateView(CreateView):
-    model = profile
+    model = Profile
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
+
+
 class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
